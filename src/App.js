662fc6fcf95b1a9dfe6f75df0adb444b309612fe
Jindřich Horváth <jindrich.horvath@unicorn.com>
2022-09-21 11:54:41 +0200

    override boostrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import './theme.sass';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import ThingView from "./bricks/ThingView";
 import {useState} from "react";
 
